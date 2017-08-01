96ae9edb754ed4409ccf216e680da231c6fc3a72
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-08-01 16:34:39 +0100

    Reworking of classes to use Printable interface, tests passing

--- Photographer.java
+++ Photographer.java
@@ -2,22 +2,30 @@ import java.util.*;
 
 public class Photographer{
 
-  private ArrayList<String> cameras;
+  private ArrayList<Printable> cameras;
 
   public Photographer(){
-    this.cameras = new ArrayList<String>();
+    this.cameras = new ArrayList<Printable>();
   } 
 
   public int countCameras(){
     return this.cameras.size();
   }
 
-  public void addCamera( String camera ){
+  public void addCamera( Printable camera ){
     this.cameras.add( camera );
   }
 
   public void removeCamera( int position ){
     this.cameras.remove( position );
+  }
+
+  public String printCameraDetails(){
+    String concatDetails = ""
+    for ( Printable camera : this.cameras ) {
+      concatDetails = concatDetails + " , " + camera.printDetails();
+    }
+    return concatDetails;
   }
 
 }
--- PhotographerTest.java
+++ PhotographerTest.java
@@ -5,10 +5,14 @@ import org.junit.Before;
 public class PhotographerTest{
 
   Photographer jill;
+  DigitalCamera nikon;
+  AnalogCamera hasselblad;
 
   @Before
   public void before(){
     jill = new Photographer();
+    nikon = new DigitalCamera( "SLR Nikon with paparazzi lens" );
+    hasselblad = new AnalogCamera( "Antique bellows Hasselblad" );
   }
 
   @Test
@@ -18,15 +22,18 @@ public class PhotographerTest{
 
   @Test
   public void addCameraToJill(){
-    jill.addCamera( "SLR Nikon with paparazzi lens" );
+    jill.addCamera( nikon );
     assertEquals( 1, jill.countCameras() );
   }
 
   @Test
   public void removeCameraFromJill(){
-    jill.addCamera( "SLR Nikon with paparazzi lens" );
+    jill.addCamera( nikon );
+    jill.addCamera( hasselblad );    
     jill.removeCamera( 0 );
-    assertEquals( 0, jill.countCameras() );
+    assertEquals( 1, jill.countCameras() );
   }
+
+
 
 }
